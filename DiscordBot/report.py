108eb765204c8df59a76a61d177267ac9dfb38ef
ship1901 <81907526+ship1901@users.noreply.github.com>
2024-05-05 21:53:46 -0700

    Beginning user-side flow for reporting message or user
    You can start reporting a message or a user. You can select an abuse type to report. Only flows that are not for impersonation go to completion so far.

--- DiscordBot/report.py
+++ DiscordBot/report.py
@@ -4,14 +4,33 @@ import re
 
 class State(Enum):
     REPORT_START = auto()
+    AWAITING_REPORT_TYPE = auto()
     AWAITING_MESSAGE = auto()
+    AWAITING_USER = auto()
     MESSAGE_IDENTIFIED = auto()
+    USER_IDENTIFIED = auto()
+    AWAITING_ABUSE_TYPE = auto()
+    AWAITING_BLOCK_DECISION = auto()
     REPORT_COMPLETE = auto()
 
 class Report:
     START_KEYWORD = "report"
     CANCEL_KEYWORD = "cancel"
     HELP_KEYWORD = "help"
+    MESSAGE_KEYWORD = "message"
+    CONVERSATION_KEYWORD = "conversation"
+    USER_KEYWORD = "user"
+    ABUSE_TYPES_DICT = {
+        "1": "harassment or bullying",
+        "2": "nudity or pornography",
+        "3": "suicide or self-harm",
+        "4": "violence or drug abuse",
+        "5": "the user may be under 13",
+        "6": "selling or promoting restricted items",
+        "7": "misleading content or scams",
+        "8": "threatening or blackmailing",
+        "9": "impersonation"
+    }
 
     def __init__(self, client):
         self.state = State.REPORT_START
@@ -25,18 +44,42 @@ class Report:
         get you started and give you a model for working with Discord. 
         '''
 
+        # User cancels report.
         if message.content == self.CANCEL_KEYWORD:
             self.state = State.REPORT_COMPLETE
             return ["Report cancelled."]
         
+        # User starts reporting process.
         if self.state == State.REPORT_START:
             reply =  "Thank you for starting the reporting process. "
             reply += "Say `help` at any time for more information.\n\n"
-            reply += "Please copy paste the link to the message you want to report.\n"
-            reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
-            self.state = State.AWAITING_MESSAGE
+            reply += "Are you reporting a message, a user, or a conversation?\n"
+            reply += "You can say `" + self.MESSAGE_KEYWORD + "`, `" + self.USER_KEYWORD + "`, or `" + self.CONVERSATION_KEYWORD + "`."
+            self.state = State.AWAITING_REPORT_TYPE
             return [reply]
         
+        # User specifies what they're reporting (message, user, conversation).
+        if self.state == State.AWAITING_REPORT_TYPE:
+            match message.content.lower():
+                case self.MESSAGE_KEYWORD:
+                    reply = "Please copy and paste the link to the message you want to report.\n"
+                    reply += "You can obtain this link by right-clicking the message and clicking `Copy Message Link`."
+                    self.state = State.AWAITING_MESSAGE
+                    return [reply]
+                case self.USER_KEYWORD:
+                    reply = "Please copy and paste the ID of the user profile you wish to report.\n"
+                    reply += "You can obtain this ID by turning on Developer Mode, right-clicking the user's name or profile picture, and clicking `Copy User ID`."
+                    self.state = State.AWAITING_USER
+                    return [reply]
+                case self.CONVERSATION_KEYWORD:
+                    reply = "Not implemented yet."
+                    return [reply]
+                case _:
+                    reply = "That is not a valid response. Please say `" + self.MESSAGE_KEYWORD + "`, `" + self.USER_KEYWORD + "`, or `" + self.CONVERSATION_KEYWORD + "`, or say `cancel` to cancel."
+                    self.state = State.AWAITING_REPORT_TYPE
+                    return [reply]
+        
+        # User is reporting a message.
         if self.state == State.AWAITING_MESSAGE:
             # Parse out the three ID strings from the message link
             m = re.search('/(\d+)/(\d+)/(\d+)', message.content)
@@ -51,15 +94,60 @@ class Report:
             try:
                 message = await channel.fetch_message(int(m.group(3)))
             except discord.errors.NotFound:
-                return ["It seems this message was deleted or never existed. Please try again or say `cancel` to cancel."]
+                return ["It seems that this message was deleted or never existed. Please try again or say `cancel` to cancel."]
 
-            # Here we've found the message - it's up to you to decide what to do next!
+            # Here we've found the message.
             self.state = State.MESSAGE_IDENTIFIED
-            return ["I found this message:", "```" + message.author.name + ": " + message.content + "```", \
-                    "This is all I know how to do right now - it's up to you to build out the rest of my reporting flow!"]
+            reply = "I found this message:" + "```" + message.author.name + ": " + message.content + "```\n"
+            reply += "What are you reporting this message for? Enter the number for the type of abuse from the list below. \n\n"
+            for key, value in self.ABUSE_TYPES_DICT.items():
+                reply += "\n" + key + ". " + value
+            self.state = State.AWAITING_ABUSE_TYPE
+            return [reply]
         
-        if self.state == State.MESSAGE_IDENTIFIED:
-            return ["<insert rest of reporting flow here>"]
+        # User is reporting a user profile.
+        if self.state == State.AWAITING_USER:
+            try:
+                user = await self.client.fetch_user(message.content)
+            except discord.errors.NotFound:
+                return ["It seems that this user profile was deleted or never existed. Please try again or say `cancel` to cancel."]
+            
+            # Here we've found the user.
+            self.state = State.USER_IDENTIFIED
+            reply = "What are you reporting " + user.name + " for? Enter the number for the type of abuse from the list below.\n"
+            for key, value in self.ABUSE_TYPES_DICT.items():
+                reply += "\n" + key + ". " + value
+            self.state = State.AWAITING_ABUSE_TYPE
+            return [reply]            
+        
+        # User specifies the abuse type.
+        if self.state == State.AWAITING_ABUSE_TYPE:
+            # Other abuse type. Shallow implementation.
+            if message.content.lower() in self.ABUSE_TYPES_DICT.keys() and self.ABUSE_TYPES_DICT[message.content] != "impersonation":
+                reply = "Thank you for your report with the listed reason of `" + self.ABUSE_TYPES_DICT[message.content]+ "`.\n\n"
+                reply += "Our content moderation team will review the report and take appropriate actions according to our Community Guidelines. Note that your report is anonymous. The account you reported will not see who reported them.\n\n"
+                reply += "Would you also like to block this user? Enter `yes` or `no`."
+                self.state = State.AWAITING_BLOCK_DECISION
+            # Impersonation
+            elif message.content.lower() in self.ABUSE_TYPES_DICT.keys() and self.ABUSE_TYPES_DICT[message.content] == "impersonation":
+                reply = "Not implemented yet."
+            else:
+                reply = "That was not a valid response. Please try again or say `cancel` to cancel."
+            return [reply]
+        
+        # User decides whether to also block the user profile they are reporting.
+        if self.state == State. AWAITING_BLOCK_DECISION:
+            match message.content.lower():
+                case "yes":
+                    reply = "Not implemented yet."
+                    self.state = State.REPORT_COMPLETE
+                case "no":
+                    reply = "Ok."
+                    self.state = State.REPORT_COMPLETE
+                case _:
+                    reply = "That is not a valid response. Please try again or say `cancel` to cancel."
+                    self.state = State.AWAITING_BLOCK_DECISION
+            return [reply]
 
         return []
 
