5740db35b9fe0a8180656565c8ca7e2b822643c9
Adam Nalley <167756408+a1372321@users.noreply.github.com>
2024-05-06 21:59:44 -0700

    Created NEW moderator.py, added ABUSE_DEFINITIONS_DICT
    Created NEW moderator.py
    Added ABUSE_DEFINITIONS_DICT for moderator to compare reported message to determine if it meets definition

---
+++ DiscordBot/moderator.py
@@ -0,0 +1,12 @@
+# Summarized Definitions derived from Meta Policy
+ABUSE_DEFINITIONS_DICT {
+
+    "Harassment or Bullying" : "The act of targeting individuals or a group of people in an to primarily cause psychological or physical harm, which includes sexualization and threat to cause psychological or physical harm."
+    "Nudity or Pornography" : "Imagery of visible human genitalia and body parts (i.e. buttocks, breasts, etc.), with exception to contexts of famine, genocide, war crimes, or crimes against humanity;\n imagery of human nudity and human sexual activity and stimulation, with exception to medical/health contexts."    
+    "Suicide or Self-Harm" : "Content that promotes, encourages, coordinates, or provides instructions for life-threatening injury that may lead to loss of life;\n Content that promotes, encourages, coordinates, or provides instructions for injury to one's self."
+    "Violence or Drug Abuse" : "Depictions of people experiencing extreme inflicted bodily injury that may lead to loss of life, including livestreams of capital punishment and sadistic remarks;\n "
+    "Restricted Items" : "Firearms, high-risk drugs, non-medical drugs, pharmaceutical drugs, marijuana, endangered species (wildlife and plants), live non-endangered animals (including livestock), human blood, alcohol/tobacco, weight loss products, historical artifacts, entheogens, hazardous goods and materials."
+    "Misleading Content or Scams" : "Content that promotes, encourages, coordinates, or provides instructions for the deception of a third party for financial/personal gain, as well as generating falsified documentation or stealing information to portray financial/personal gain."  
+    "Threatening or Blackmailing" : "Content that promotes, encourages, coordinates, or provides instructions for the conspiracy or pledge to cause harm against people, animals, or property, which include the exposure of identity and/or personal information in a forceful manner."
+    "Impersonation" : "The act of creating, repurposing, or otherwise using an account that deliberately misrepresents your identity in order to mislead or deceive others, evade enforcement of policy, or otherwise assuming to be or speak for another person or entity for whom the user is not authorized to do so."
+}
